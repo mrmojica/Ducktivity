2cdab70015a5507b53784b2072101a5be3661adb
mrmojica <mrmojica510@gmail.com>
2017-03-17 13:28:15 -0700

    fixed objective null error

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    (process.env.NODE_ENV = 'mongodb://ducktivity:ducktivity123@ds139267.mlab.com:39267/ducktivity');
+    (process.env.NODE_ENV = 'mongodb://ducktivity:ducktivity123@ds045882.mlab.com:45882/ducktivty');
 exports.PORT = process.env.PORT || 8080;
